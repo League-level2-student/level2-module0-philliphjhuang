fada7b4f69bb0c7fbb539f0c20b2a75dfb5bc313
arushmshah <league@WTS20.attlocal.net>
2018-10-18 16:59:29 -0700

    arrays
    .

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -4,34 +4,45 @@ import java.util.Random;
 
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
-		String[] names = {"a","b","c","d","e"};
-		//2. print the third element in the array
+		// 1. make an array of 5 Strings
+		String[] names = { "a", "b", "c", "d", "e" };
+		// 2. print the third element in the array
 		System.out.println(names[2]);
-		//3. set the third element to a different value
+		// 3. set the third element to a different value
 		names[2] = "x";
-		//4. print the third element again
+		// 4. print the third element again
 		System.out.println(names[2]);
-		//5. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		for(int i = 0; i< names.length; i++) {
+		// 5. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < names.length; i++) {
 			System.out.println(names[i]);
 		}
-		//6. make an array of 50 integers
+		// 6. make an array of 50 integers
 		int[] nums = new int[50];
-		//7. use a for loop to make every value of the integer array a random number
+		// 7. use a for loop to make every value of the integer array a random number
 		Random r = new Random();
-		for(int z = 0 ; z < nums.length ; z++) {
+		for (int z = 0; z < nums.length; z++) {
 			int x = r.nextInt(49);
-			nums[z]=x;
+			nums[z] = x;
 		}
-		//8. without printing the entire array, print only the smallest number in the array
-		for(int m = 0 ; m < nums.length ; m++) {
-			
-		
+		// 8. without printing the entire array, print only the smallest number in the
+		// array
+		int c = nums[0];
+		int b = nums[49];
+		for (int m = 1; m < nums.length; m++) {
+			if (nums[m] < c) {
+				c = nums[m];
+				System.out.println(c);
+			}
+
+			// 9 print the entire array to see if step 8 was correct
+			System.out.println(nums[m]);
+
+			// 10. print the largest number in the array.
+			if (nums[m] > b) {
+				b = nums[m];
+				System.out.print(b);
+			}
 		}
-		//9 print the entire array to see if step 8 was correct
-		
-		//10. print the largest number in the array.
 	}
 }
